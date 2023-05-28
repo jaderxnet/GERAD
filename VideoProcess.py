f0406fbd37771044e562de06c7a6207dbe45f91d
jaoa <jaoa@cin.ufpe.br>
2023-05-28 16:15:06 -0300

    Add YOLOv8

--- VideoProcess.py
+++ VideoProcess.py
@@ -22,7 +22,12 @@ Create Python class to process each video  in processedVideos.csv
 6 - Save a picture in the first frame by sequence;
 '''
 
-
+from ultralytics import YOLO
+from mediapipe.tasks.python import vision
+from mediapipe.tasks import python
+import mediapipe as mp
+import os.path
+import json
 import numpy as np
 from mediapipe.framework.formats import landmark_pb2
 from mediapipe import solutions
@@ -32,11 +37,12 @@ from yt_dlp import YoutubeDL
 from datetime import datetime
 #!pip install "opencv-python-headless<4.3"
 import cv2
+
+# YOLOv8 pip install ultralytics
+import ultralytics
+ultralytics.checks()
 # Dowload python > 3.9 version in https://www.python.org/downloads
 #!pip install -q mediapipe==0.10.0
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 print("MEDIAPIPE: ", mp.__version__)
 
 
@@ -62,6 +68,7 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 
 
 inputFilePath = "processedVideos.csv"
+
 
 videosTable = pd.read_csv(inputFilePath, sep=';')
 print("Lendo: ", inputFilePath)
@@ -117,12 +124,32 @@ else:
             base_options=BaseOptions(model_asset_path=model_path_Heavy),
             running_mode=VisionRunningMode.VIDEO)
 
+        # Load a model
+        model = YOLO('yolov8n-pose.pt')  # load an official model
+        # model = YOLO('path/to/best.pt')  # load a custom model
+
         # Use OpenCV’s VideoCapture to load the input video.
         cap = cv2.VideoCapture("videos/" + selectedVideo['id']
                                + "/" + selectedVideo['id']
                                + ".mp4")
+        outputFilePath = "/Users/jaderxnet/Documents/GitHub/GERAD/videos/" + \
+            selectedVideo['id'] + "/" + selectedVideo['id']+".txt"
+        file1 = open(outputFilePath, "w")
+        file1.write("Inicio " + outputFilePath)
         if (cap.isOpened() == False):
             print("Error opening video stream or file")
+
+        dictionary = {}
+        # dictionary with video id on youtube
+        dictionary[selectedVideo["id"]] = {}
+        dictionary[selectedVideo["id"]]["duration"] = selectedVideo["duration"]
+        dictionary[selectedVideo["id"]]["fps"] = selectedVideo["fps"]
+        # frames have the dictionary with frame id
+        dictionary[selectedVideo["id"]]["frames"] = {}
+
+        print(dictionary)
+        print_count = 0
+        frames_count = 1
 
         # Read until video is completed
         while (cap.isOpened()):
@@ -133,10 +160,22 @@ else:
 
                 # Display the resulting frame
                 # cv2.imshow('Video Frame', frame)
+                # Predict with the model
+                results = model(frame)  # predict on an image
+                print("Quant restults: ", len(results))
+                # if (print_count < 1 and len(results[0].keypoints) > 0):
+                # print("Yolo Results: ",  results)
+                # print("Keypoints Results: ",  results[0].keypoints)
+                res_plotted = results[0].plot()
+                # cv2.imshow("result", res_plotted)
+                # print("YOLO Results:", results)
 
                 # Convert the frame received from OpenCV to a MediaPipe’s Image object.
                 mp_image = mp.Image(
                     image_format=mp.ImageFormat.SRGB, data=frame)
+                mp_image_yolo = mp.Image(
+                    image_format=mp.ImageFormat.SRGB, data=res_plotted)
+
                 with PoseLandmarker.create_from_options(options) as landmarker:
                     # The landmarker is initialized. Use it here.
                     # ...
@@ -147,15 +186,33 @@ else:
                         mp_image, frame_timestamp_ms)
                     quantidade_poses = len(
                         pose_landmarker_result.pose_landmarks)
-                    print(quantidade_poses)
+                    # frames have the dictionary with frame id
+                    dictionary[selectedVideo["id"]
+                               ]["frames"][frames_count] = {}
+                    # mediapipe have the dictionary  neural networks detections
+                    dictionary[selectedVideo["id"]
+                               ]["frames"][frames_count]["mediapipe"] = {}
+                    dictionary[selectedVideo["id"]
+                               ]["frames"][frames_count]["mediapipe"]["neural_network_file"] = model_path_Heavy
+                    # landmarks have the landmarks
+                    dictionary[selectedVideo["id"]
+                               ]["frames"][frames_count]["mediapipe"]["landmarks"] = pose_landmarker_result.pose_landmarks
+                    frames_count += 1
+                    print("\033[K", f'{frames_count:06}', end="\r")
+                    if (quantidade_poses > 0 and print_count < 3):
+                        # print(dictionary)
+                        print_count += 1
+                        print(quantidade_poses)
+
                     if quantidade_poses > 0:
                         # print("Pose:", pose_landmarker_result)
                         annotated_image = draw_landmarks_on_image(
-                            mp_image.numpy_view(), pose_landmarker_result)
+                            mp_image_yolo.numpy_view(), pose_landmarker_result)
                         annotated_image_rgb = cv2.cvtColor(
                             annotated_image, cv2.COLOR_RGB2BGR)
                         # print("Image: ", annotated_image)
-                        # cv2.imshow("MediaPipe Fame", annotated_image_rgb)
+                        cv2.imshow("MediaPipe Fame", annotated_image_rgb)
+
                 # Press Q on keyboard to  exit
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
@@ -165,7 +222,9 @@ else:
 
         # When everything done, release the video capture object
         cap.release()
-
+        print(dictionary)
+        dictionary[selectedVideo["id"]
+                   ]["frames_count"] = frames_count-1
         # Closes all the frames
         cv2.destroyAllWindows()
 
@@ -173,3 +232,20 @@ else:
         # You’ll need it to calculate the timestamp for each frame.
 
         # Loop through each frame in the video using VideoCapture#read()
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+
+if (file1 is not None):
+    file1.write(str(dictionary))
+
+    file1.close()
